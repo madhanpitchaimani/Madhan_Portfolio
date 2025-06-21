3eae9275279bb38bcdf76e19a07fc5e9c7480a7d
Madhan.P <madhanpitchaimani@gmail.com>
2025-06-21 11:42:08 +0530

    Updated styles and layout

--- my-react-app/src/Components/Navbar.jsx
+++ my-react-app/src/Components/Navbar.jsx
@@ -4,16 +4,17 @@ import './navbar.css';
 function Navbar() {
   return (
     <nav>
-      <h1>Madhan</h1>
-      <div className="links" >
-        <a href="#about">About</a>
-        <a href="#education">Education</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+      <div className="nav-container">
+        <h1>Madhan</h1>
+        <div className="links">
+          <a href="#about">About</a>
+          <a href="#education">Education</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
-
